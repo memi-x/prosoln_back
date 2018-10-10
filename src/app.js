82e07934df57188a4c335e4338800f646dc9f2db
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2018-10-10 16:48:49 +0300

    feature(signup-login): write endpoint for signup and login - modularized the application - add sign up and sign in controller

--- src/app.js
+++ src/app.js
@@ -3,35 +3,26 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import env from './config/environments';
-import User from './models/user';
+import routes from './modules';
+// import UserController from './modules/user/userController';
 
 const app = express();
-const PORT = env.PORT || 8090;
-const apiPrefix = '/api/v1';
-const Router = express.Router();
 // Connect database from .env file
 mongoose.connect(env.DATABASE_URL);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-Router.use((req, res, next) => {
-  console.log('The first route to be called');
-  next();
-});
-
-Router.route('/user').post((req, res) => {
-  let newUser = new User();
-  newUser.name = req.body.name;
-  newUser.save(error => {
-    if (error) {
-      res.send(error);
-    }
-    res.send('Registered successfully');
-  });
-});
-
 app.use(cors());
-app.use(apiPrefix, Router);
+const PORT = env.PORT || 8090;
+
+routes(app);
+// app.post('/register', UserController.registerUser);
+app.use('*', (req, res) =>
+  res.status(404).json({
+    message: 'The url is not found in our system, make sure you add /api/v1/.'
+  })
+);
+
 app.listen(PORT);
+
 console.log('app is listening to port', PORT);
--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  password: { type: String },
+  memberSince: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', UserSchema);
